f57cf93e0c514ae8396b3a90bc1536fead1e6b32
SonuKumar81800 <sonu.patna0808@gmail.com>
2020-10-01 10:31:21 +0530

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./App.css";
 import Sidebar from "./Sidebar";
@@ -9,7 +9,7 @@ import { useStateValue } from "./StateProvider";
 
 function App() {
 
-  const [{ user }, dispatch] = useStateValue();
+  const [{ user }] = useStateValue();
 
   return (
     <div className="app">
